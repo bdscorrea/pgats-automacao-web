8093ed0fddd63b73134d1e132eae8a67a3b03225
BEATRIZ CORRÊA <66252245+bdscorrea@users.noreply.github.com>
2025-10-20 14:00:05 -0300

    adicionando retrise

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retrise: {
+    openMode: 0,
+    runMode: 2,
+  },
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
